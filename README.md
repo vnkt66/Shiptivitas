afd6a4405b2dbaf71a6b1a2e838118068da3994e
venkanna penubothu <venkanna.penubothu@gmail.com>
2020-06-29 13:05:09 +0530

    Modified Board according to backend

---
+++ README.md
@@ -0,0 +1,7 @@
+## Explaination
+
+Used Dragula to implement drag and drop functionality using the refs
+provided in the file.
+
+Used switch case to know the target swimlane for changing the class through
+Iteartion of the HtmlCollection by creating a array from it.
--- src/Board.js
+++ src/Board.js
@@ -1,97 +1,195 @@
-import React from 'react';
-import Dragula from 'dragula';
-import 'dragula/dist/dragula.css';
-import Swimlane from './Swimlane';
-import './Board.css';
+import React from "react";
+import Dragula from "dragula";
+import "dragula/dist/dragula.css";
+import Swimlane from "./Swimlane";
+import "./Board.css";
+// import { data } from 'jquery';
+import axios from "axios";
 
 export default class Board extends React.Component {
-  
-    constructor() {
+  constructor() {
     super();
-    const clients = this.getClients();
     this.state = {
+      clientsraw: [],
       clients: {
-        backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
-        complete: clients.filter(client => client.status && client.status === 'complete'),
+        backlog: [],
+        inProgress: [],
+        complete: []
       }
-    }
+    };
     this.swimlanes = {
       backlog: React.createRef(),
       inProgress: React.createRef(),
-      complete: React.createRef(),
-    }
+      complete: React.createRef()
+    };
   }
   getClients() {
-    return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'in-progress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
-    ].map(companyDetails => ({
-      id: companyDetails[0],
-      name: companyDetails[1],
-      description: companyDetails[2],
-      status: companyDetails[3],
-    }));
+    axios
+      .get("http://localhost:3001/api/v1/clients")
+      .then(res => {
+        let data = res.data;
+        this.setState({
+          clientsraw: data,
+          clients: {
+            backlog: data
+              .filter(client => !client.status || client.status === "backlog")
+              .sort((a, b) => (a.priority > b.priority ? 1 : -1)),
+            inProgress: data
+              .filter(
+                client => client.status && client.status === "in-progress"
+              )
+              .sort((a, b) => (a.priority > b.priority ? 1 : -1)),
+            complete: data
+              .filter(client => client.status && client.status === "complete")
+              .sort((a, b) => (a.priority > b.priority ? 1 : -1))
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  updateClient(id, priority, status, siblingId, array) {
+    var arrLength = array.length;
+    // console.log(arrLength);
+    var arr = [...array];
+    if (siblingId !== -1) {
+      // console.log("siblingId", "end", siblingId, arrLength);
+      // console.log(array);
+      var siblingIdArray = [];
+      for (let i = siblingId; i < arrLength; i++) {
+        siblingIdArray.push(arr[i].getAttribute("data-id"));
+      }
+    }
+    axios
+      .put(`http://localhost:3001/api/v1/clients/${id}`, {
+        status,
+        priority,
+        siblingId,
+        length: arrLength,
+        siblingIdArray
+      })
+      .then(res => {
+        console.log(res);
+        // can add alerts
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   renderSwimlane(name, clients, ref) {
-    return (
-      <Swimlane name={name} clients={clients} dragulaRef={ref}/>
-    );
+    return <Swimlane name={name} clients={clients} dragulaRef={ref} />;
   }
-
   componentDidMount() {
     const backlog = this.swimlanes.backlog.current;
     const progress = this.swimlanes.inProgress.current;
     const complete = this.swimlanes.complete.current;
-    Dragula(
-      [backlog, progress, complete], 
-      {revertOnSpill: true}
-      )
-      .on('drag', function (el) {
-      }).on('drop', function (el, target, source, sibling) {
-        switch(target) {
+    const Board = this;
+    this.getClients();
+    Dragula([backlog, progress, complete], { revertOnSpill: true })
+      .on("shadow", function(el, container) {
+        // console.log(el, container);
+        // console.log(el.dataId);
+      })
+      .on("drag", function(el) {
+        // console.log('drag');
+        // console.log(el);
+      })
+      .on("drop", function(el, target, source, sibling) {
+        console.log("drop");
+        // console.log('sibling', sibling);
+        // var siblingId = sibling ? sibling.getAttribute('data-id') : null;
+        // console.log('siblingIndex', siblingIndex);
+        // let clientIndex = siblingIndex - 1;
+        // console.log(clientIndex);
+        switch (target) {
           case backlog:
+            el.setAttribute("data-status", "backlog");
+            el.setAttribute("class", "Card Card-grey");
             let backlogArray = Array.from(backlog.children);
-            backlogArray.forEach((el) => {
-              el.className = 'Card Card-grey';
+            // console.log("BacklogChildren", backlogArray.length);
+            let backlogElIndex = backlogArray.findIndex(backlogel => {
+              return backlogel === el;
             });
+            // console.log(sibling);
+            let siblingIndex = backlogArray.findIndex(bkel => {
+              return sibling === bkel;
+            });
+            let id = el.getAttribute("data-id");
+            let status = el.getAttribute("data-status");
+            // console.log("backlogEl", el);
+            // console.log("backlogSiblingIndex", siblingIndex);
+            // console.log("ID", el.getAttribute("data-id"));
+            // console.log("Prority", backlogElIndex + 1);
+            // console.log("Status", status);
+            Board.updateClient(
+              id,
+              backlogElIndex + 1,
+              status,
+              siblingIndex,
+              backlogArray
+            );
             break;
           case progress:
+            el.setAttribute("data-status", "in-progress");
+            el.setAttribute("class", "Card Card-blue");
             let progressArray = Array.from(progress.children);
-            progressArray.forEach((el) => {
-              el.className = 'Card Card-blue';
+            // console.log("ProgressChildren", progressArray.length);
+            let progressElIndex = progressArray.findIndex(progressEl => {
+              return progressEl === el;
             });
+            // console.log(sibling);
+            let psiblingIndex = progressArray.findIndex(pel => {
+              return sibling === pel;
+            });
+            let pid = el.getAttribute("data-id");
+            // let psiblingId = sibling.getAttribute('data-id');
+            let pstatus = el.getAttribute("data-status");
+            // console.log("progressEl", el);
+            // console.log("progressSiblingId", psiblingIndex);
+            // console.log("ID", pid);
+            // console.log("Prority", progressElIndex + 1);
+            // console.log("Status", pstatus);
+            Board.updateClient(
+              pid,
+              progressElIndex + 1,
+              pstatus,
+              psiblingIndex,
+              progressArray
+            );
             break;
           case complete:
+            el.setAttribute("data-status", "complete");
+            el.setAttribute("class", "Card Card-green");
             let completeArray = Array.from(complete.children);
-            completeArray.forEach((el) => {
-              el.className = 'Card Card-green';
+            // console.log("CompleteChildren", completeArray.length);
+            let completeElIndex = completeArray.findIndex(completeEl => {
+              return completeEl === el;
             });
+            let csiblingIndex = completeArray.findIndex(cel => {
+              return sibling === cel;
+            });
+            let cid = el.getAttribute("data-id");
+            // let csiblingId = sibling.getAttribute('data-id');
+            let cstatus = el.getAttribute("data-status");
+            // console.log("completeEl", el);
+            // console.log("completeSiblingId", csiblingIndex);
+            // console.log("ID", el.getAttribute("data-id"));
+            // console.log("Priority", completeElIndex + 1);
+            // console.log("Status", cstatus);
+            Board.updateClient(
+              cid,
+              completeElIndex + 1,
+              cstatus,
+              csiblingIndex,
+              completeArray
+            );
             break;
           default:
-            console.log('default');
+            console.log("default");
         }
       });
   }
-      
 
   render() {
     return (
@@ -99,13 +197,25 @@ export default class Board extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-4">
-              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog)}
+              {this.renderSwimlane(
+                "Backlog",
+                this.state.clients.backlog,
+                this.swimlanes.backlog
+              )}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress)}
+              {this.renderSwimlane(
+                "In Progress",
+                this.state.clients.inProgress,
+                this.swimlanes.inProgress
+              )}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete)}
+              {this.renderSwimlane(
+                "Complete",
+                this.state.clients.complete,
+                this.swimlanes.complete
+              )}
             </div>
           </div>
         </div>
